19f1df49e8b03a39271af07bc06ff0524f984113
Samuelson23 <3samuelmg@gmail.com>
2023-05-22 22:58:50 +0200

    event routes

--- src/api/routes/Event.routes.js
+++ src/api/routes/Event.routes.js
@@ -1,4 +1,11 @@
-const create = require("../controllers/Event.controllers");
+const {
+  create,
+  updateEvent,
+  deleteEvent,
+  getAll,
+  getById,
+  getByName,
+} = require("../controllers/Event.controllers");
 
 const express = require("express");
 const eventRoutes = express.Router();
