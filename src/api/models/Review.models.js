ab48edcf89285156608ff15541c24c403d9641b5
Samuelson23 <3samuelmg@gmail.com>
2023-05-22 21:42:13 +0200

    Review model

--- src/api/models/Review.models.js
+++ src/api/models/Review.models.js
@@ -1,30 +1,21 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const validator = require('validator');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
-const UserSchema = new mongoose.Schema(
- {
-   
-    event: [{type: mongoose.Schema.Types.ObjectId, ref: "Event", required: true, unique: true} ]
-    ,
-    description: {
-        type: String,
-        required: true,
-    },
-    user: {
-        type: [{type: mongoose.Schema.Types.ObjectId, ref: "Event", required: true, unique: true} ]
-        
-    },
-    points: {
-        type: Number,
-        required: true,
+const ReviewSchema = new mongoose.Schema({
+  event: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
+  description: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
+  },
+  points: {
+    type: Number,
+    required: true,
+  },
+});
 
-    }
-}
-);
-
-
-
-
-
-   
+const Review = mongoose.model("Review", ReviewSchema);
+module.exports = Review;
