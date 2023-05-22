c807b792bf7f7a94f81575625dc5f22423d082ec
Samuelson23 <3samuelmg@gmail.com>
2023-05-22 22:29:08 +0200

    index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const { configCloudinary } = require("./src/middleware/files.middleware");
 const { connect } = require("./src/utils/db");
 const express = require("express");
 const dotenv = require("dotenv");
+
 const server = express();
 
-dotenv.config()
+dotenv.config();
 configCloudinary();
+const ROUTE = process.env.ROUTE;
 const PORT = process.env.PORT;
 connect();
 
@@ -17,19 +19,20 @@ server.use(express.urlencoded({ limit: "5mb", extended: true }));
                         ROUTES
 
 */
-
+const eventRoutes = require("./src/api/routes/Event.routes");
+server.use("/api/v1/event/", eventRoutes);
 
 //controlamos los errores cuando no se meta bien la ruta o haya algun fallo
 server.use("*", (req, res, next) => {
-    const error = new Error("Route not found");
-    error.status = 404;
-    return next(error);
+  const error = new Error("Route not found");
+  error.status = 404;
+  return next(error);
 });
-server.use((error,req,res)=>{
-    return res.status(error.status||500).json(error.message||"Unexpected error")
-})
+server.use((error, req, res) => {
+  return res.status(error.status || 500).json(error.message || "Unexpected error");
+});
 
-server.disable("x-powered-by")
+server.disable("x-powered-by");
 server.listen(PORT, () => {
-    console.log(`Listening on PORT http://localhost:${PORT}`);
-  });
+  console.log(`Listening on PORT ${ROUTE}${PORT}`); //Metemos la URL de forma dinámica por si en un futuro hubieran cambios de dirección
+});
