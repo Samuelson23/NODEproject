41757cd6c6f3feda83c0fc5dc4db51284d947ebe
Samuelson23 <3samuelmg@gmail.com>
2023-05-22 19:45:32 +0200

    utils comentados

--- src/utils/randomCode.js
+++ src/utils/randomCode.js
@@ -1,6 +1,8 @@
+//creamos una funcion que nos genera un codigo de verificacion random para enviarlo por email a la hora de registrarse el usuario
+
 const randomCode = () => {
-    let code = Math.floor(Math.random() * (999999 - 100000) + 100000);
-    return code;
-  };
-  
-  module.exports = randomCode;
+  let code = Math.floor(Math.random() * (999999 - 100000) + 100000);
+  return code;
+};
+
+module.exports = randomCode;
--- src/utils/token.js
+++ src/utils/token.js
@@ -1,18 +1,21 @@
-const jwt = require("jsonwebtoken")
-const dotenv = require("dotenv")
-dotenv.config()
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const generateToken=(id,email)=>{
-    if(!id||!email){
-        throw new Error("No se ha encontrado ID o email")
+//Creamos una función que si existe el ID y el email nos creará un token para poder acceder
+const generateToken = (id, email) => {
+  if (!id || !email) {
+    throw new Error("No se ha encontrado ID o email");
+  }
+  //con jwt.sign firmamos el token para ese ID y email y le ponemos el tiempo de duracion
+  return jwt.sign({ id, email }, process.env.JWT_SECRET, { expiresIn: "1d" });
+};
 
-    }
-    return jwt.sign({id,email}, process.env.JWT_SECRET, {expiresIn:"1d"})
-}
-
-const verifyToken = (token) =>{
-    if(!token){
-        throw new Error("No hay token")
-    }
-    return jwt.verify(token,process.env.JWT_SECRET)
-}
+//Creamos una funcion para verificar el token, a la cual le pasamos el token como parametro para que haga un jwt.verify
+const verifyToken = (token) => {
+  if (!token) {
+    throw new Error("No hay token");
+  }
+  //con jwt.verify verificamos que los datos del token sean permitidos por la JWT_SECRET (no es lo mismo)
+  return jwt.verify(token, process.env.JWT_SECRET);
+};
