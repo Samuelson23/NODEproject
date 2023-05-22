fc2127bc8fc2d7fcdd0a18b11d8d8e45a3a25ca6
Samuelson23 <3samuelmg@gmail.com>
2023-05-22 22:59:29 +0200

    user routes

---
+++ src/api/routes/User.routes.js
@@ -0,0 +1,14 @@
+const {
+  register,
+  getAll,
+  getById,
+  getByName,
+  forgotPassword,
+  changePassword,
+  login,
+  logout,
+  updateUser,
+  deleteUser,
+  createEvent,
+  addToEvent,
+} = require("../controllers/User.controllers");
