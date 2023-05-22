d5c4f5844c9fd60b2aa3fe66dfae79676f8898f2
Samuelson23 <3samuelmg@gmail.com>
2023-05-22 22:27:57 +0200

    user model

--- src/api/models/User.models.js
+++ src/api/models/User.models.js
@@ -1,40 +1,46 @@
 const mongoose = require("mongoose");
-
+const validator = require("validator");
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
     name: { type: String, required: false, unique: true },
     gender: {
-        type: String,
-        enum: ["hombre", "mujer"],
-        required: true,
+      type: String,
+      enum: ["hombre", "mujer"],
+      required: true,
     },
     role: {
-        type: String,
-        enum: ["user", "admin"],
-        required: true,
+      type: String,
+      enum: ["user", "admin"],
+      required: true,
     },
-    email: { type: String, required: true, unique: true, validate:[validator.isEmail, 'Email not valid'] },
-    password: { type: String, required: true, trim: true, validate:[validator.isStrongPassword] },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validator.isEmail, "Email not valid"],
+    },
+    password: { type: String, required: true, trim: true, validate: [validator.isStrongPassword] },
     imagen: { type: String },
     confirmationCode: { type: String, required: true },
     check: { type: Boolean, default: false },
     events: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
-    review: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }]
-    },
-    {
-        timestamps: true,
-    }
+    review: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
+  },
+  {
+    timestamps: true,
+  }
 );
 
-UserSchema.pre('save', async function (next) {
-    try {
-      this.password = await bcrypt.hash(this.password, 10);
-      next();
-    } catch (error) {
-      next('Error hashing password', error);
-    }
-  });
+UserSchema.pre("save", async function (next) {
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  } catch (error) {
+    next("Error hashing password", error);
+  }
+});
 
 const User = mongoose.model("User", UserSchema);
 module.exports = User;
